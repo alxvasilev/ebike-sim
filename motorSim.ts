91a73116b6611ca2125e4d35d2b6128fba2a6fca
Alexander Vassilev <av@mega.co.nz>
2021-10-14 20:21:00 +0300

    Working approximate model with I0

--- motorSim.ts
+++ motorSim.ts
@@ -8,6 +8,7 @@ class Ebike {
     motorWindingR!: number;
     motorKv!: number;
     motorKt!: number;
+    motorI0!: number;
 
     battVoltage!: number;
     battR!: number;
@@ -28,6 +29,7 @@ class Ebike {
     constructor(config: {
         motorWindingR: number,
         motorKv: number,
+        motorI0: number,
         battVoltage: number,
         battR: number;
         battMaxI: number;
@@ -42,6 +44,9 @@ class Ebike {
         this.motorKt = (60 / (2 * Math.PI * this.motorKv));
         this.wheelCircumf = 2 * Math.PI * this.wheelRadius;
     }
+    reset() {
+        this.curRpm = 0;
+    }
     tick(period: number) { // 1ms tick
         let force = this.curForce();
         let accel = force / this.weight;
@@ -54,11 +59,11 @@ class Ebike {
     }
     dragForce() {
         let speed = this.curSpeed();
-        let weightForce = this.weight * 9.8;
-        let drag = (1.2 * (speed ** 2) * this.cdA) / 2; // air drag
+        let weightForce = this.weight * 9.81;
+        let drag = (1.204 * (speed ** 2) * this.cdA) / 2; // air drag, air density at 20C
         if (speed > 0.001) {
-            // this.slope is the sine of the grade andle
-            drag += 2 * (Math.sqrt(1 - this.slope**2) * weightForce * this.wheelRR); // rolling resistance of two wheels
+            // this.slope is the sine of the grade angle, we need the cosine
+            drag += Math.sqrt(1 - this.slope**2) * weightForce * this.wheelRR; // rolling resistance of two wheels
         }
         drag += this.slope * weightForce; // slope
         return drag;
@@ -69,24 +74,23 @@ class Ebike {
     curForce() {
         let motorBackEmf = this.curRpm / this.motorKv;
         let totalR = this.systemR + this.motorWindingR;
-
-        let motorI = (this.throttle*this.battVoltage - motorBackEmf) / (totalR + this.battR*(this.throttle**2));
-        if (motorI < 0.0) {
-            motorI = 0.0;
+        let motorIm = -(motorBackEmf + this.battR*(this.throttle**2)*this.motorI0 - this.throttle*this.battVoltage) / (totalR + this.battR*(this.throttle**2));
+        if (motorIm < 0.0) {
+            motorIm = 0.0;
         }
-        let battI = motorI * this.throttle;
+        let battI = motorIm * this.throttle; // ignore I0 because it will make throttle equation very complex
 
         if ((battI > this.battMaxI) || (battI < this.battMaxI && this.throttle < this.targetThrottle)) {
             let throttle = -(Math.sqrt(motorBackEmf**2 + 4*this.battMaxI*totalR*(this.battVoltage-this.battMaxI*this.battR)) + motorBackEmf) / (2*this.battMaxI*this.battR - 2*this.battVoltage);
             this.setEffThrottle(throttle);
 
-            motorI = (this.throttle*this.battVoltage - motorBackEmf) / (totalR + this.battR*(this.throttle**2));
-            battI = motorI * this.throttle;
+            motorIm = -(motorBackEmf + this.battR*(this.throttle**2)*this.motorI0 - this.throttle*this.battVoltage) / (totalR + this.battR*(this.throttle**2));
+            battI = motorIm * this.throttle;
         }
         assert(battI <= this.battMaxI + 0.000001);
-        this.curBattI = battI;
-        this.curMotorI = motorI;
-        this.curTorque = motorI * this.motorKt;
+        this.curMotorI = motorIm + this.motorI0;
+        this.curBattI = this.curMotorI * this.throttle;
+        this.curTorque = motorIm * this.motorKt;
         return this.curTorque / this.wheelRadius - this.dragForce();
     }
     curEfficiency() {
@@ -114,19 +118,20 @@ function tenth(val: number) {
 
 if (typeof window === "undefined") {
     let ebike = new Ebike({
-        motorWindingR: 0.15,
+        motorWindingR: 0.2,
         motorKv: 7.231,
+        motorI0: 1.15,
         battVoltage: 60,
-        battR: 0.143,
+        battR: 0.15,
         battMaxI: 28,
         systemR: 0.1,
-        wheelRadius: 0.368,
+        wheelRadius: 0.3685,
         wheelRR: 0.008,
-        cdA: 0.6,
+        cdA: 0.64,
         weight: 110,
         slope: 0.0
     });
-    ebike.setThrottle(0.6);
+    ebike.setThrottle(0.36);
     setInterval(() => {
         let speed = ebike.tick(0.010);
         let mechP = ebike.curTorque * (ebike.curRpm / 60) * 2 * Math.PI;
