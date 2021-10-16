5ffbbf0f6829690b0dc970a3575797bafba1f916
Alexander Vassilev <av@mega.co.nz>
2021-10-16 16:59:13 +0300

    Implemented multiple graph series

--- chart.js
+++ chart.js
@@ -1,21 +1,42 @@
 "use strict";
+var Series = /** @class */ (function () {
+    function Series(name, maxVal, color, ctx) {
+        this.name = name;
+        if (color.charAt(0) !== '#') {
+            throw new Error("Color must start with #");
+        }
+        var colorVal = parseInt(color.substr(1), 16);
+        if (isNaN(colorVal)) {
+            throw new Error("Error parsing color hex value");
+        }
+        this.color = color;
+        this.yScale = ctx.canvas.height / (maxVal + 1);
+        this.scalingY = (Math.abs(this.yScale - 1.0) > 0.00001);
+        var pix = this.pixel = ctx.createImageData(1, 1);
+        var data = pix.data;
+        data[0] = colorVal >> 24;
+        data[1] = (colorVal >> 16) & 0xff;
+        data[2] = (colorVal >> 8) & 0xff;
+        data[3] = 255;
+    }
+    return Series;
+}());
 var LiveChart = /** @class */ (function () {
     function LiveChart(canvas, maxY) {
         this.valCount = 0;
+        this.series = [];
         this.canvas = canvas;
         this.ctx = canvas.getContext("2d");
-        this.yScale = canvas.height / maxY;
-        this.scalingY = (Math.abs(this.yScale - 1.0) > 0.00001);
-        if (this.scalingY) {
-            //            this.ctx.scale(1, yScale);
-        }
         this.valMaxCount = canvas.width;
-        var pix = this.blackPixel = this.ctx.createImageData(1, 1);
-        var data = pix.data;
-        data[0] = data[1] = data[2] = 0;
-        data[3] = 255;
     }
+    LiveChart.prototype.addSeries = function (name, color, maxVal) {
+        this.series.push(new Series(name, maxVal, color, this.ctx));
+    };
     LiveChart.prototype.addValues = function (sample) {
+        var series = this.series;
+        if (sample.length < series.length) {
+            throw new Error("Missing values for all series");
+        }
         if (this.valCount >= this.valMaxCount) {
             this.scrollCanvas();
         }
@@ -30,19 +51,19 @@ var LiveChart = /** @class */ (function () {
         var ctx = this.ctx;
         var height = this.canvas.height;
         var x = this.valCount - 1;
-        var yScale = this.yScale;
-        if (this.scalingY) {
-            this.ctx.beginPath();
-            for (var i = 0; i < sample.length; i++) {
+        for (var i = 0; i < series.length; i++) {
+            var ser = series[i];
+            if (ser.scalingY) {
+                var yScale = ser.yScale;
+                this.ctx.beginPath();
+                ctx.strokeStyle = series[i].color;
                 this.ctx.moveTo(x - 1, height - this.prevSample[i] * yScale - 1);
                 this.ctx.lineTo(x, height - sample[i] * yScale - 1);
+                ctx.closePath();
+                ctx.stroke();
             }
-            ctx.closePath();
-            ctx.stroke();
-        }
-        else {
-            for (var i = 0; i < sample.length; i++) {
-                this.ctx.putImageData(this.blackPixel, x, Math.round(height - sample[i] - 1));
+            else {
+                this.ctx.putImageData(series[i].pixel, x, Math.round(height - sample[i] - 1));
             }
         }
     };
--- chart.ts
+++ chart.ts
@@ -1,4 +1,31 @@
 type Values = Array<number>;
+class Series {
+    name: string;
+    color: string;
+    pixel: ImageData;
+    yScale: number;
+    scalingY: boolean;
+    constructor(name: string, maxVal: number, color: string, ctx: CanvasRenderingContext2D) {
+        this.name = name;
+        if (color.charAt(0) !== '#') {
+            throw new Error("Color must start with #");
+        }
+        let colorVal = parseInt(color.substr(1), 16);
+        if (isNaN(colorVal)) {
+            throw new Error("Error parsing color hex value");
+        }
+        this.color = color;
+        this.yScale = ctx.canvas.height / (maxVal+1);
+        this.scalingY = (Math.abs(this.yScale - 1.0) > 0.00001);
+
+        let pix = this.pixel = ctx.createImageData(1, 1);
+        let data = pix.data;
+        data[0] = colorVal >> 24;
+        data[1] = (colorVal >> 16) & 0xff;
+        data[2] = (colorVal >> 8) & 0xff;
+        data[3] = 255;
+    }
+}
 
 class LiveChart {
     valCount: number = 0;
@@ -7,24 +34,20 @@ class LiveChart {
     lastSample?: Values;
     canvas: HTMLCanvasElement;
     ctx: CanvasRenderingContext2D;
-    blackPixel: ImageData;
-    yScale: number;
-    scalingY: boolean;
+    series: Series[] = [];
     constructor(canvas: HTMLCanvasElement, maxY: number) {
         this.canvas = canvas;
         this.ctx = canvas.getContext("2d")!;
-        this.yScale = canvas.height / maxY;
-        this.scalingY = (Math.abs(this.yScale - 1.0) > 0.00001);
-        if (this.scalingY) {
-//            this.ctx.scale(1, yScale);
-        }
         this.valMaxCount = canvas.width;
-        let pix = this.blackPixel = this.ctx.createImageData(1, 1);
-        let data = pix.data;
-        data[0] = data[1] = data[2] = 0;
-        data[3] = 255;
+    }
+    addSeries(name: string, color: string, maxVal: number) {
+        this.series.push(new Series(name, maxVal, color, this.ctx));
     }
     addValues(sample: Values) {
+        let series = this.series;
+        if (sample.length < series.length) {
+            throw new Error("Missing values for all series");
+        }
         if (this.valCount >= this.valMaxCount) {
             this.scrollCanvas();
         } else {
@@ -38,18 +61,18 @@ class LiveChart {
         let ctx = this.ctx;
         let height = this.canvas.height;
         let x = this.valCount-1;
-        let yScale = this.yScale;
-        if (this.scalingY) {
-            this.ctx.beginPath();
-            for (let i = 0; i < sample.length; i++) {
+        for (let i = 0; i < series.length; i++) {
+            let ser = series[i];
+            if (ser.scalingY) {
+                let yScale = ser.yScale;
+                this.ctx.beginPath();
+                ctx.strokeStyle = series[i].color;
                 this.ctx.moveTo(x-1, height - this.prevSample[i]*yScale - 1);
                 this.ctx.lineTo(x, height - sample[i] * yScale - 1);
-            }
-            ctx.closePath();
-            ctx.stroke();
-        } else {
-            for (let i = 0; i < sample.length; i++) {
-                this.ctx.putImageData(this.blackPixel, x, Math.round(height - sample[i]-1));
+                ctx.closePath();
+                ctx.stroke();
+            } else {
+                this.ctx.putImageData(series[i].pixel, x, Math.round(height - sample[i]-1));
             }
         }
     }
